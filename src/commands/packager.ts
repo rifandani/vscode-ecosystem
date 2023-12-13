00b45edb4b35e6624f1396e4a52c2ffb5d9b3e59
rifandani <ipandani2505@gmail.com>
2023-12-13 14:06:27 +0700

    feat(packager): add "veco.packager.updateAll" command

--- src/commands/packager.ts
+++ src/commands/packager.ts
@@ -2,4 +2,5 @@ export const commandIds = {
   refreshEntry: 'veco.packager.refreshEntry',
   link: 'veco.packager.link',
   remove: 'veco.packager.remove',
+  updateAll: 'veco.packager.updateAll',
 } as const
