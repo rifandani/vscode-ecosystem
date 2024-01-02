3887403c6421a217791e5cb64a9ee863734b5aa2
rifandani <ipandani2505@gmail.com>
2024-01-02 09:29:14 +0700

    fix(snippets): move folders from src to root

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: [
-    'src/index.ts',
-  ],
+  entry: ['src/index.ts'],
   format: ['cjs'],
+  external: ['vscode'],
+  clean: true,
   shims: false,
   dts: false,
   sourcemap: true,
-  external: [
-    'vscode',
-  ],
 })
